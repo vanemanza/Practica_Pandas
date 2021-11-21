70bf2ee9cbdcf8c10d6d21ec81facac11e23d651
VaneManzanelli <vanemanza1@gmail.com>
2021-11-21 20:49:02 -0300

    acceder a elem x colum, mediante key o atr

---
+++ DataFrame/acceso_a_datos.py
@@ -0,0 +1,12 @@
+"""
+Obtener informacion de un DF x una columna:
+"""
+
+# 1) Accedo a la columna, mediante llave o atr
+"""
+df.nombre
+df['nombre']
+df.nombre[0]
+df.calificacion[ df.calificacion > 9]
+"""
+
