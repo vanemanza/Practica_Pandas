f711d6fa31d5a47345c89c8aedbaad729ab7ff70
VaneManzanelli <vanemanza1@gmail.com>
2021-11-22 21:15:18 -0300

    nuevas consultas de Clash Royale

--- CONSULTAS/ClashRoyal/main.py
+++ CONSULTAS/ClashRoyal/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 
-datos = pd.read_csv('clashrds.csv')
+datos = pd.read_csv('../DATA/clashrds.csv')
 #print(datos)
 
 df = pd.DataFrame(datos, columns=['Card', 'Cost', 'Count', 'Damage', 'Damage/sec', 'DeathDamage', 'Health', 'HitSpeed', 'Level', 'MaxSpawned', 'Radius', 'Range', 'SpawnDPS', 'SpawnDamage', 'SpawnHealth', 'SpawnSpeed', 'SpawnerHealth', 'TroopSpawned', 'Type'])
@@ -9,3 +10,23 @@ df['Count'][20] = '3'
 
 df['Count'] = pd.to_numeric(df['Count'])
 df['Damage'] = pd.to_numeric(df['Damage'])
+
+"""
+Quiero cambiar los indices para q empiecen desde el 1
+"""
+df.index = list(np.arange(1,71))
+
+"""
+Quiero ver q tipo de dato es cada columna
+"""
+
+for column in df.columns:
+    tipo = df[column].dtype
+    print(f'{column} = {tipo}')
+    
+"""
+Agrupar por tipo:
+"""
+Tropas = df.loc[df.Type == 'Troops and Defenses']
+Hechizos = df.loc[df.Type == 'Damaging Spells']
+Spawners = df.loc[df.Type == 'Spawners']
